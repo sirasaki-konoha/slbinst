252a2c8b631f8e80fbc0e14e9c6b4e8e39666b6d
sirasaki-konoha <rock-db@outlook.jp>
2025-09-04 15:45:37 +0900

    add help message

--- src/main.c
+++ src/main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 #include "download_src.h"
 
-int main() {
-	printf("Hello, world!");
+
+void help(const char* call_name) {
+	printf(
+			"slbinst version %s\n"
+			"Usage:\n"
+			"\t%s install [PACKAGE]\n"
+			"\t%s update [PACKAGE]\n"
+			"\t%s search [PACKAGE]\n"
+	      , PACKAGE_VERSION, call_name, call_name, call_name);
+	return;
+}
+
+
+int main(int argc, char* argv[]) {
+	if (argc < 2) {
+		help(argv[0]);
+	}
 }
